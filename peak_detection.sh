57f23638eb20a1b008436ee19b9414e8bda7496d
jorainer <johannes.rainer@gmail.com>
2020-10-26 19:04:28 +0100

    fix shell script

--- peak_detection.sh
+++ peak_detection.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 PTH=`pwd`
-/shared/bioinf/R/bin/R-4.0.0-BioC3.11 -e 'rmarkdown::render(\"peak_detection.R\")'
+/shared/bioinf/R/bin/R-4.0.0-BioC3.11 -e 'rmarkdown::render(\"peak_detection.Rmd\")'
