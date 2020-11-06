a4da86328690dec338109871bc6a4f4a84aa93a6
jorainer <johannes.rainer@gmail.com>
2020-11-06 10:35:12 +0100

    Update to Bioconductor 3.12

--- peak_detection.sh
+++ peak_detection.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 PTH=`pwd`
-/shared/bioinf/R/bin/R-4.0.0-BioC3.11 -e 'rmarkdown::render(\"peak_detection.Rmd\")'
+/shared/bioinf/R/bin/R-4.0-BioC3.12 -e 'rmarkdown::render(\"peak_detection.Rmd\")'
