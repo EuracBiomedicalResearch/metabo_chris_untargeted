b6e519b0dc33a47ed67965c074ecf1fadeb66991
mdgrv <github@mare.science>
2024-10-10 14:20:47 +0200

    50percent per batch cv below 0.3 change info html reportx

--- normalization.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-/shared/bioinf/R/R-3.6.1-BioC3.10/bin/R --file="/home/mgarciaaloy/git/EuracBiomedicalResearch/metabo_chris_untargeted/normalization.R"
--- peak_detection.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-PTH=`pwd`
-/shared/bioinf/R/bin/R-4.1-BioC3.13 -e 'rmarkdown::render(\"peak_detection.Rmd\")'
